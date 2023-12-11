ce35857712714e8a6f13d153e43f0baab05f9038
algabl <103858110+algabl@users.noreply.github.com>
2023-12-11 09:17:12 -0700

    usability, added download to calendar function

--- src/Meeting/MeetingMain.js
+++ src/Meeting/MeetingMain.js
@@ -1,6 +1,11 @@
 import {
   Button,
   Container,
+  Dialog,
+  DialogActions,
+  DialogContent,
+  DialogContentText,
+  DialogTitle,
   Paper,
   Table,
   TableBody,
@@ -8,7 +13,9 @@ import {
   TableContainer,
   TableHead,
   TableRow,
+  Tooltip,
 } from "@mui/material";
+import { DurationInput, createEvent } from 'ics';
 import React, { useState } from "react";
 
 import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
@@ -16,6 +23,7 @@ import AddIcon from '@mui/icons-material/Add';
 import DeleteIcon from '@mui/icons-material/Delete';
 import EditIcon from '@mui/icons-material/Edit';
 import { IconButton } from "@mui/material";
+import InsertInvitationIcon from '@mui/icons-material/InsertInvitation';
 import { LocalizationProvider } from "@mui/x-date-pickers";
 import MeetingScheduler from "./MeetingScheduler";
 import dayjs from "dayjs";
@@ -24,6 +32,8 @@ const MeetingMain = () => {
   const [meetings, setMeetings] = useState([]);
   const [editingMeeting, setEditingMeeting] = useState(null);
   const [open, setOpen] = useState(false);
+  const [openDeleteDialog, setOpenDeleteDialog] = useState(false);
+  const [deleteMeetingIndex, setDeleteMeetingIndex] = useState(null);
 
   
   function addMeeting(date, counselor, note) {
@@ -32,6 +42,10 @@ const MeetingMain = () => {
 
   const handleClickOpen = () => {
     setOpen(true);
+  }
+
+  const handleClickDeleteOpen = () => {
+    setOpenDeleteDialog(true);
   }
 
   function handleEditOpen(index) {
@@ -47,6 +61,16 @@ const MeetingMain = () => {
     setEditingMeeting(null);
   }
 
+  const handleDeleteConfirmation = (index) => {
+    handleDelete(index);
+    setOpenDeleteDialog(false);
+  };
+  
+  const handleCancelDelete = () => {
+    // Close delete dialog without performing delete action
+    setOpenDeleteDialog(false);
+  };
+
   function handleSave(date, counselor, note) {
     if (editingMeeting) {
       const index =meetings.findIndex(meeting => meeting === editingMeeting);
@@ -60,19 +84,49 @@ const MeetingMain = () => {
     }
   }
 
+  function downloadIcsFile(meeting) {
+    const event = {
+      start: [meeting.date.year(), meeting.date.month() + 1, meeting.date.date(), meeting.date.hour(), meeting.date.minute()],
+      duration: { hours: 1 }, // adjust as needed
+      title: 'Meeting with ' + meeting.counselor,
+      description: meeting.note,
+      organizer: { name: 'Meeting Scheduler', email: 'scheduler@example.com' }, // adjust as needed
+      attendees: [
+        { name: meeting.counselor, email: 'counselor@example.com' } // adjust as needed
+      ],
+    };
+  
+    createEvent(event, (error, value) => {
+      if (error) {
+        console.log(error);
+        return;
+      }
+  
+      const blob = new Blob([value], { type: 'text/calendar;charset=utf-8' });
+      const link = document.createElement('a');
+      link.href = URL.createObjectURL(blob);
+      link.download = 'meeting.ics';
+      document.body.appendChild(link);
+      link.click();
+      document.body.removeChild(link);
+    });
+  }
+
   return (
     <LocalizationProvider dateAdapter={AdapterDayjs}>
       
       <Container sx={{ padding: '20px' }} maxWidth="sm">
         <MeetingScheduler open={open} setOpen={setOpen} saveMeeting={handleSave} meeting={editingMeeting}/>
-        <Button
-          variant="outlined"
-          startIcon={<AddIcon />}
-          onClick={handleClickOpen}
-          sx={{ marginBottom: '20px' }}
-          >
-          Schedule Meeting
-        </Button>
+        <Tooltip title="Schedule Meeting">
+          <Button
+            variant="outlined"
+            startIcon={<AddIcon />}
+            onClick={handleClickOpen}
+            sx={{ marginBottom: '20px' }}
+            >
+            Schedule Meeting
+          </Button>
+        </Tooltip>
         <TableContainer component = {Paper}>
           <Table sx={{ minWidth:500}} >
             <TableHead>
@@ -80,6 +134,7 @@ const MeetingMain = () => {
                 <TableCell>Date</TableCell>
                 <TableCell>Time</TableCell>
                 <TableCell>Counselor</TableCell>
+                <TableCell></TableCell>
                 <TableCell></TableCell>
                 <TableCell></TableCell>
               </TableRow>
@@ -100,14 +155,42 @@ const MeetingMain = () => {
                     {meeting.counselor}
                   </TableCell>
                   <TableCell component="th" scope="meeting">
-                    <IconButton onClick={() => handleEditOpen(index)}>
-                      <EditIcon />
-                    </IconButton>
+                    <Tooltip title="Add to Calendar">
+                      <IconButton onClick={() => downloadIcsFile(meeting)}>
+                        <InsertInvitationIcon/>
+                      </IconButton>
+                    </Tooltip>
                   </TableCell>
                   <TableCell component="th" scope="meeting">
-                    <IconButton onClick={() => handleDelete(index)}>
-                      <DeleteIcon />
-                    </IconButton>
+                    <Tooltip title="Edit">
+                      <IconButton onClick={() => handleEditOpen(index)}>
+                        <EditIcon />
+                      </IconButton>
+                    </Tooltip>
+                  </TableCell>
+                  <TableCell component="th" scope="meeting">
+                    <Tooltip title="Delete">
+                      <IconButton onClick={() => handleClickDeleteOpen(index)}>
+                        <DeleteIcon />
+                      </IconButton>
+                    </Tooltip>
+                    <Dialog
+                      open={openDeleteDialog}
+                      onConfirm={handleDeleteConfirmation}
+                      onCancel={handleCancelDelete}
+                      index={deleteMeetingIndex}
+                    >
+                      <DialogTitle>Delete Meeting</DialogTitle>
+                      <DialogContent>
+                        <DialogContentText>
+                          Are you sure you want to delete this meeting?
+                        </DialogContentText>
+                      </DialogContent>
+                      <DialogActions>
+                        <Button onClick={handleCancelDelete}>Cancel</Button>
+                        <Button onClick={() => handleDeleteConfirmation(deleteMeetingIndex)}>Delete</Button>
+                      </DialogActions>
+                    </Dialog>
                   </TableCell>
                 </TableRow>
               ))}
--- src/Meeting/MeetingScheduler.js
+++ src/Meeting/MeetingScheduler.js
@@ -11,7 +11,7 @@ import {
   NativeSelect,
   TextField,
 } from "@mui/material";
-import React, { Fragment, useState } from "react";
+import React, { Fragment, useEffect, useState } from "react";
 
 import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
 import AddIcon from "@mui/icons-material/Add";
@@ -19,7 +19,8 @@ import { LocalizationProvider } from "@mui/x-date-pickers";
 import { StaticDateTimePicker } from "@mui/x-date-pickers";
 import dayjs from "dayjs";
 
-const MeetingScheduler = ({ saveMeeting, meeting, open, setOpen }) => {
+const MeetingScheduler = (props) => {
+  const { saveMeeting, meeting, open, setOpen } = props;
   const [date, setDate] = useState(meeting ? meeting.date : dayjs());
   const [counselor, setCounselor] = useState(meeting ? meeting.counselor : "");
   const [note, setNote] = useState(meeting ? meeting.note : "");
@@ -32,12 +33,19 @@ const MeetingScheduler = ({ saveMeeting, meeting, open, setOpen }) => {
     "Kylee Holmes",
     "Erik Velez",
   ];
-
+  // Inside MeetingScheduler component
+  useEffect(() => {
+    setCounselor(meeting ? meeting.counselor : "");
+  }, [meeting]);
+  
   const handleSave = () => {
     saveMeeting(date, counselor, note);
   };
 
   const handleClose = () => {
+    setDate(dayjs());
+    setCounselor("");
+    setNote("");
     setOpen(false);
   };
 
@@ -69,28 +77,27 @@ const MeetingScheduler = ({ saveMeeting, meeting, open, setOpen }) => {
                 Counselor
               </InputLabel>
               <NativeSelect
-                defaultValue={counselor}
+                value={counselor}
                 inputProps={{
                   name: "counselor",
                   id: "uncontrolled-native",
                 }}
                 onChange={(e) => setCounselor(e.target.value)}
               >
-                <option value="" disabled hidden>
-                </option>
+                <option value="" disabled hidden></option>
                 {counselors.map((listcounselor) => (
                   <option value={listcounselor}>{listcounselor}</option>
                 ))}
               </NativeSelect>
             </FormControl>
             <TextField
-              sx={{ mt: 2}}
+              sx={{ mt: 2 }}
               multiline
               rows={4}
               fullWidth
               label="Appointment notes"
               defaultValue={note}
-              onChange={(e) => setNote(e.target.value) }
+              onChange={(e) => setNote(e.target.value)}
             />
           </DialogContent>
           <DialogActions>
